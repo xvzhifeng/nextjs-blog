25d0b52fb8cab065d905107f29220de4115ffa44
xuzhif <zhifeng.xu@hpe.com>
2022-06-23 17:36:41 +0800

    security style

--- pages/security/index.js
+++ pages/security/index.js
@@ -5,6 +5,7 @@ import React from "react";
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router'
 import md5 from 'js-md5';
+import { Grid } from "@mui/material";
 
 export default function app() {
 
@@ -17,10 +18,10 @@ export default function app() {
     }
     let submit = () => {
         let data1 = md5(data)
-        fetch('/api/security').then(res => res.json()).then(res=>{
-            if(res.results.length > 0 && data1 == res.results[0].auth_code && !localStorage.getItem("upload")) {
+        fetch('/api/security').then(res => res.json()).then(res => {
+            if (res.results.length > 0 && data1 == res.results[0].auth_code && !localStorage.getItem("upload")) {
                 localStorage.setItem("upload", data1)
-                fetch('/api/security/add').then(res => res.json()).then(res=>{
+                fetch('/api/security/add').then(res => res.json()).then(res => {
                     console.log(res);
                     router.push("/upload")
                 })
@@ -31,15 +32,27 @@ export default function app() {
             console.log(res.results.length)
             // console.log(res)
         })
-        
+
     }
 
     return (
         <Layout>
-            <h1>security</h1>
-            <TextField fullWidth label="security" id="password" onChange={(e) => handleChange(e)} />
-            <br />
-            <Button id="btn_submit" variant="outlined" onClick={submit}>submit</Button>
+            <Grid container spacing={2}>
+                <Grid item xs={12}></Grid>
+                <Grid item xs={2}> <h1>security</h1></Grid>
+                <Grid item xs={8}> </Grid>
+                <Grid item xs={2}></Grid>
+                <Grid item xs={2}></Grid>
+                <Grid item xs={12}>
+                    <TextField fullWidth label="security" id="password" onChange={(e) => handleChange(e)} />
+                </Grid>
+                <Grid item xs={12}></Grid>
+                <Grid item xs={4} ></Grid>
+                <Grid item xs={2}><Button id="btn_submit" variant="outlined" onClick={submit}>submit</Button></Grid>
+                <Grid item xs={5}>
+                </Grid>
+                <Grid item xs={2}></Grid>
+            </Grid>
         </Layout>
 
     )
